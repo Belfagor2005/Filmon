4115deacfb2a0e4e47bf5370a239e384bfc15228
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2022-10-11 22:58:48 +0200

    Update Update.py

--- usr/lib/enigma2/python/Plugins/Extensions/Filmon/Update.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Filmon/Update.py
@@ -3,12 +3,12 @@
 
 import os
 import sys
-from twisted.web.client import downloadPage
 PY3 = sys.version_info.major >= 3
 print("Update.py")
 
 
 def upd_done():
+    from twisted.web.client import downloadPage
     print("In upd_done")
     xfile = 'http://patbuweb.com/filmon/filmon.tar'
     if PY3:
@@ -28,10 +28,10 @@ def upd_done():
 
 
 def upd_last(fplug):
+    import os
     import time
     time.sleep(5)
     if os.path.isfile('/tmp/filmon.tar') and os.stat('/tmp/filmon.tar').st_size > 10000:
-        import os
         cmd = "tar -xvf /tmp/filmon.tar -C /"
         print("cmd A =", cmd)
         os.system(cmd)
