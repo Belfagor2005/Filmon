93f825c5958132734be874e4cd86cbb44d17bee6
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2023-09-21 20:33:01 +0200

    Update plugin.py

--- usr/lib/enigma2/python/Plugins/Extensions/Filmon/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Filmon/plugin.py
@@ -714,11 +714,7 @@ class Playstream2(
         self.onClose.append(self.cancel)
 
     def getAspect(self):
-        try:
-            aspect = iAVSwitch().getAspectRatioSetting()
-        except:
-            aspect = eAVSwitch().getAspectRatioSetting()
-        return aspect
+        return AVSwitch().getAspectRatioSetting()
 
     def getAspectString(self, aspectnum):
         return {0: '4:3 Letterbox',
@@ -739,9 +735,9 @@ class Playstream2(
                6: '16_9_letterbox'}
         config.av.aspectratio.setValue(map[aspect])
         try:
-            iAVSwitch.setAspectRatio(aspect)
+            AVSwitch().setAspectRatio(aspect)
         except:
-            eAVSwitch.setAspectRatio(aspect)
+            pass
 
     def av(self):
         temp = int(self.getAspect())
